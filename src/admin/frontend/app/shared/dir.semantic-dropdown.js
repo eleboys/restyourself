b23d65bf5c3878f8d2890925cbc4e9e63c04111e
Nima Baghdadi <bg.nima@gmail.com>
2017-08-02 15:21:43 +0430

    tag dropdown bug fix

--- src/admin/frontend/app/shared/dir.semantic-dropdown.js
+++ src/admin/frontend/app/shared/dir.semantic-dropdown.js
@@ -20,15 +20,17 @@
 
     function link(scope, element, attrs, ngModelCtrl) {
 			var options = scope.options || {};
-			angular.extend(options, {
-				onChange: function (value, text) {
-					if (!ngModelCtrl) return;
-					if (!value || !value.trim())
-						ngModelCtrl.$setViewValue([]);
-					else
-						ngModelCtrl.$setViewValue(value.split(','));
-				}
-			})
+			if (ngModelCtrl && element[0].nodeName === "DIV") {
+				angular.extend(options, {
+					onChange: function (value, text) {
+						if (!ngModelCtrl) return;
+						if (!value || !value.trim())
+							ngModelCtrl.$setViewValue([]);
+						else
+							ngModelCtrl.$setViewValue(value.split(','));
+					}
+				})
+			}
       setTimeout(function () {
 				element.dropdown(options);
 				if (options.allowAdditions && ngModelCtrl.$modelValue){
